43188ed1022b81ee5f47107dbb56c60306289610
Sven Neumann <sven.neumann@holoplot.com>
2024-09-10 10:14:13 +0200

    client: Print message rate when done

--- client/result.go
+++ client/result.go
@@ -10,6 +10,10 @@ type Result struct {
 	Elapsed          time.Duration
 }
 
+func (r *Result) Rate() float64 {
+	return float64(r.ConsumedMessages) / r.Elapsed.Seconds()
+}
+
 func (r *Result) String() string {
-	return fmt.Sprintf("Consumed %d messages in %s", r.ConsumedMessages, r.Elapsed)
+	return fmt.Sprintf("Consumed %d messages in %s (%.1f msgs/sec)", r.ConsumedMessages, r.Elapsed, r.Rate())
 }
